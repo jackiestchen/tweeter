bc540b33c9faf9dce63cdf1f945ef6b34fc83d74
jackiestchen <jackiestchen@gmail.com>
2021-11-27 16:25:07 -0700

    Implement character counter in new-tweet form

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,15 @@
+$(document).ready(function () {
+  //selecting textarea element in form classname new-tweet and listening to input event
+  $(".new-tweet form textarea").on("input change", function () {
+    // console.log(this.value.length);
+    let charCount = this.value.length;
+    let charRemaining = 140 - charCount;
+    const output = $(this).siblings("div").children("output");
+
+    if (charRemaining < 0) {
+      output.css("color", "red").text(charRemaining);
+    } else {
+      output.css("color", "initial").text(charRemaining);
+    }
+  });
+});
