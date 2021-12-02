d352987ff49e9a42cb86a469c83404f8ede82955
jackiestchen <jackiestchen@gmail.com>
2021-12-02 07:41:51 -0700

    Add load tweet features

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -3,30 +3,6 @@
  * jQuery is already loaded
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
-// const data = [
-//   {
-//     user: {
-//       name: "Newton",
-//       avatars: "https://i.imgur.com/73hZDYK.png",
-//       handle: "@SirIsaac",
-//     },
-//     content: {
-//       text: "If I have seen further it is by standing on the shoulders of giants",
-//     },
-//     created_at: 1461116232227,
-//   },
-//   {
-//     user: {
-//       name: "Descartes",
-//       avatars: "https://i.imgur.com/nlhLi3I.png",
-//       handle: "@rd",
-//     },
-//     content: {
-//       text: "Je pense , donc je suis",
-//     },
-//     created_at: 1461113959088,
-//   },
-// ];
 
 const createTweetElement = (object) => {
   let $tweet = `
@@ -42,7 +18,7 @@ const createTweetElement = (object) => {
           ${object.content.text}
         </p>
         <footer>
-          <div class="last-tweet-day">${getTime(object.created_at)}</div>
+          <div class="last-tweet-day">${timeago.format(object.created_at)}</div>
           <div class="tweet-icons">
             <i class="fa-solid fa-flag"></i>
             <i class="fa-solid fa-retweet"></i>
@@ -57,41 +33,9 @@ const createTweetElement = (object) => {
 const renderTweets = (tweets) => {
   for (const el of tweets) {
     let $tweet = createTweetElement(el);
-    $(".tweet-container").append($tweet);
+    $(".tweet-container").prepend($tweet);
   }
 };
-
-const getTime = (created) => {
-  const now = new Date().getTime();
-  const howLongAgo = created - now;
-  const time = Math.abs(howLongAgo);
-
-  let humanTime = 0;
-  let units = "";
-  if (time > 1000 * 60 * 60 * 24 * 365) {
-    humanTime = parseInt(time / (1000 * 60 * 60 * 24 * 365), 10);
-    units = "years";
-  } else if (time > 1000 * 60 * 60 * 24 * 30) {
-    humanTime = parseInt(time / (1000 * 60 * 60 * 24 * 30), 10);
-    units = "months";
-  } else if (time > 1000 * 60 * 60 * 24 * 7) {
-    humanTime = parseInt(time / (1000 * 60 * 60 * 24 * 7), 10);
-    units = "weeks";
-  } else if (time > 1000 * 60 * 60 * 24) {
-    humanTime = parseInt(time / (1000 * 60 * 60 * 24), 10);
-    units = "days";
-  } else if (time > 1000 * 60 * 60) {
-    humanTime = parseInt(time / (1000 * 60 * 60), 10);
-    units = "hours";
-  } else if (time > 1000 * 60) {
-    humanTime = parseInt(time / (1000 * 60), 10);
-    units = "seconds";
-  }
-
-  return `${humanTime} ${units} ago`;
-};
-
-// renderTweets(data);
 
 $("#new-tweet-form").submit(function (event) {
   const data = $(this).serialize();
@@ -110,7 +54,6 @@ $(document).ready(function () {
     $.ajax({
       url: "/tweets",
       type: "GET",
-      // dataType: "JSON",
       success: function (response) {
         renderTweets(response);
       },
