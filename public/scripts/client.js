8f7e97ee001034f0118d0674a197ae542d418405
jackiestchen <jackiestchen@gmail.com>
2021-12-16 21:44:55 -0700

    Move helper functions to a separate file

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -3,28 +3,27 @@
  * jQuery is already loaded
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
+import { escape } from "./helpers.js";
 
-const escape = (str) => {
-  let div = document.createElement("div");
-  div.appendChild(document.createTextNode(str));
-  return div.innerHTML;
-};
-
-const createTweetElement = (object) => {
-  let $tweet = `
-  <article class="tweet-article">
+$(document).ready(function () {
+  const createTweetElement = (object) => {
+    const { user, content, created_at } = object;
+    const { avatars, name, handle } = user;
+    const { text } = content;
+    return `
+      <article class="tweet-article">
         <header>
           <div id="avatar">
-            <img src="${object.user.avatars}"/>
-            ${object.user.name}
+            <img src="${avatars}"/>
+            ${name}
           </div>
-          <div class="tweet-name">${object.user.handle}</div>
+          <div class="tweet-name">${handle}</div>
         </header>
         <p class="tweet-body">
-          ${escape(object.content.text)}
+          ${escape(text)}
         </p>
         <footer>
-          <div class="last-tweet-day">${timeago.format(object.created_at)}</div>
+          <div class="last-tweet-day">${timeago.format(created_at)}</div>
           <div class="tweet-icons">
             <i class="fa-solid fa-flag"></i>
             <i class="fa-solid fa-retweet"></i>
@@ -33,40 +32,47 @@ const createTweetElement = (object) => {
         </footer>
       </article>
   `;
-  return $tweet;
-};
+  };
 
-const renderTweets = (tweet) => {
-  let $tweet = createTweetElement(tweet);
-  $(".tweet-container").prepend($tweet);
-};
+  const renderTweets = (tweet) => {
+    let $tweet = createTweetElement(tweet);
+    $(".tweet-container").prepend($tweet);
+  };
 
-const loadTweets = function () {
-  $.get("/tweets").then((tweets) => {
-    tweets.forEach((tweet) => {
-      renderTweets(tweet);
+  const loadTweets = function () {
+    $.get("/tweets").then((tweets) => {
+      tweets.forEach((tweet) => {
+        renderTweets(tweet);
+      });
     });
-  });
-};
+  };
+  loadTweets();
 
-const tweetMessage = $(".tweet-error");
-const tweetErrorText = $(".tweet-error .message span");
-tweetMessage.hide();
+  const tweetTextarea = $(".new-tweet form textarea");
+  const tweetMessage = $(".tweet-error");
+  const tweetBtn = $("#tweet-submit-button");
+  const tweetErrorText = $(".tweet-error .message span");
+  tweetMessage.hide();
 
-$("#new-tweet-form").submit(function (event) {
-  event.preventDefault();
-  const data = $(this).serialize();
-  const length = $.trim(tweetTextarea.val()).length;
+  $("#new-tweet-form").submit(function (event) {
+    event.preventDefault();
+    const data = $(this).serialize();
+    console.log(data);
+    const length = $.trim(tweetTextarea.val()).length;
 
-  if (length > 140) {
-    tweetErrorText.text("Text must be less than or equal to 140 characters!");
-    tweetMessage.slideDown("fast");
-    $("#tweet-submit-button").addClass('button-error');
-  } else if (length < 1) {
-    tweetErrorText.text("Text must be not be empty!");
-    tweetMessage.slideDown("fast");
-    $("#tweet-submit-button").addClass('button-error');
-  } else {
+    if (length > 140) {
+      tweetErrorText.text("Text must be less than or equal to 140 characters!");
+      tweetMessage.slideDown("fast");
+      tweetBtn.addClass("button-error");
+      return;
+    }
+
+    if (length < 1) {
+      tweetErrorText.text("Text must be not be empty!");
+      tweetMessage.slideDown("fast");
+      tweetBtn.addClass("button-error");
+      return;
+    }
     $.ajax({
       type: "POST",
       url: "/tweets",
@@ -82,13 +88,7 @@ $("#new-tweet-form").submit(function (event) {
       },
     });
     tweetMessage.slideUp("fast");
-    $("#tweet-submit-button").removeClass('button-error');
+    tweetBtn.removeClass("button-error");
+  });
 
-  }
-});
-
-const tweetTextarea = $(".new-tweet form textarea");
-
-$(document).ready(function () {
-  loadTweets();
 });
